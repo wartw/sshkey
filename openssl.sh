232034de1b8a3c84b4c1e10fa247217588b387a3
黃柏澄(風雨) <wartw@users.noreply.github.com>
2024-06-16 20:26:04 +0800

    Create openssl.sh

---
+++ openssl.sh
@@ -0,0 +1,17 @@
+yum install -y gcc make perl wget perl-IPC-Cmd ssmtp
+wget https://www.openssl.org/source/openssl-3.3.1.tar.gz
+tar -xf openssl-3.3.1.tar.gz
+cd openssl-3.3.1
+./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl
+make
+make install
+echo "/usr/local/openssl/lib" > /etc/ld.so.conf.d/openssl.conf
+ldconfig
+ln -s /usr/local/openssl/lib/libssl.so.3 /usr/lib/libssl.so.3
+ln -s /usr/local/openssl/lib64/libssl.so.3 /usr/lib64/libssl.so.3
+ln -s /usr/local/openssl/lib/libcrypto.so.3 /usr/lib/libcrypto.so.3
+ln -s /usr/local/openssl/lib64/libcrypto.so.3 /usr/lib64/libcrypto.so.3
+mv /usr/bin/openssl /usr/bin/openssl.old
+ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/openssl/include/openssl /usr/include/openssl
+openssl version
