cecd3470386c919e142b04082a30259091695918
黃柏澄(風雨) <wartw@users.noreply.github.com>
2024-06-16 20:32:57 +0800

    Update openssl.sh

--- openssl.sh
+++ openssl.sh
@@ -7,6 +7,10 @@ make
 make install
 echo "/usr/local/openssl/lib" > /etc/ld.so.conf.d/openssl.conf
 ldconfig
+make
+make install
+echo "/usr/local/openssl/lib" > /etc/ld.so.conf.d/openssl.conf
+ldconfig
 ln -s /usr/local/openssl/lib/libssl.so.3 /usr/lib/libssl.so.3
 ln -s /usr/local/openssl/lib64/libssl.so.3 /usr/lib64/libssl.so.3
 ln -s /usr/local/openssl/lib/libcrypto.so.3 /usr/lib/libcrypto.so.3
